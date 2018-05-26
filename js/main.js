88af7f8fdf9378cb8ace120b4b46a611512a10a0
Goergi Kolev <hcs1993@abv.bg>
2018-05-26 15:37:03 +0300

    Final

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
-	slickSlider();
-	brandsSlider();
-});
+
+
+
+
 function slickSlider() {
 
   $('.sliderImages').slick({
@@ -24,3 +24,74 @@ function brandsSlider() {
 
   });
 }
+
+
+function newprod() {
+
+  $('.newprod').slick({
+  infinite: true,
+  dots: false,
+  arrows: true,
+   slidesToShow: 4,
+  slidesToScroll: 1
+
+
+  });
+}
+
+
+function freqprod() {
+
+  $('.freqprod').slick({
+  infinite: true,
+  dots: false,
+  arrows: true,
+   slidesToShow: 4,
+  slidesToScroll: 1
+
+
+  });
+}
+
+    
+
+
+
+
+
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+
+
+}
+
+// Get the element with id="defaultOpen" and click on it
+function defaultOpen(){
+document.getElementById("defaultOpen").click();
+}
+
+$(document).ready(function() {
+  defaultOpen();
+  newprod();
+  freqprod();
+  slickSlider();
+  brandsSlider();
+  first();
+  openCity();
+
+
+
+});
